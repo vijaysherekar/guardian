31dc42e17aff16610188f99bb5b3df69e9c414b9
Vishal Grover <vishalgrover.work@gmail.com>
2022-04-23 22:51:23 +0530

    removed log

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const DB = process.env.DATABASE.replace("<USERNAME>", process.env.USER).replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD
 );
-console.log("🚀 ~ file: server.js ~ line 15 ~ DB", DB)
 const PORT = process.env.PORT | 8000;
+
 // The below event handlers can be added before or after startServer function
 // As long as its after the require mongoose import
 mongoose.connection.once("open", () => {
