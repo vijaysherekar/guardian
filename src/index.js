396f36330a346b4238591a25177fc8445f766482
Vishal Grover <vishalgrover.work@gmail.com>
2022-04-23 18:39:20 +0530

    Connected App with the MongoDB Atlas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const http = require("http");
+
 const app = express();
-const port = 3000;
+const server = http.createServer(app);
+const PORT = 3000;
+const MONGO_URL =
+  "mongodb+srv://dbadmin:Wissen2022@medicine.k728h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// The below event handlers can be added before or after startServer function
+// As long as its after the require mongoose import
+mongoose.connection.once("open", () => {
+  console.log("Mongo DB Connection Ready");
 });
 
-app.listen(port, () => {
-  console.log(`Guardian App is running on ${port}!`);
-});
+mongoose.connection.on("error", (err)=>{
+console.error(err)
+})
+
+async function startServer() {
+  await mongoose.connect(MONGO_URL);
+  server.listen(PORT, () => {
+    console.log(`Listening on port`, PORT);
+  });
+}
+
+startServer();
